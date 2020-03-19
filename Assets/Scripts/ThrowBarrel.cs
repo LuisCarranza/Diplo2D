d4c01d58e0c5d0afc041f27dd59e3e0f7a6b7843
Luis Carranza <luisal3195@yahoo.com.mx>
2020-03-18 19:01:47 -0600

    Partial Progress, Backup

--- Assets/Scripts/ThrowBarrel.cs
+++ Assets/Scripts/ThrowBarrel.cs
@@ -16,14 +16,20 @@ public class ThrowBarrel : MonoBehaviour
 
     IEnumerator createBarrel()
     {
-        // tmpBarrel = Instantiate(barrel, barrelPos.position, Quaternion.identity);
-        tmpBarrel = ObjectPool.instance.GetGameObjectOfType("barrel_0", true);
-        if (tmpBarrel != null)
-        {
-            tmpBarrel.transform.position = barrelPos.position;
-            tmpBarrel.transform.rotation = Quaternion.identity;
-            tmpBarrel.GetComponent<Rigidbody2D>().AddForce(barrelDirection);
-        }
+        tmpBarrel = Instantiate(barrel, barrelPos.position, Quaternion.identity);
+        tmpBarrel.transform.position = barrelPos.position;
+        tmpBarrel.transform.rotation = Quaternion.identity;
+        tmpBarrel.GetComponent<Rigidbody2D>().AddForce(barrelDirection);
+
+        //From object pool
+        // tmpBarrel = ObjectPool.instance.GetGameObjectOfType("barrel_0", true);
+        // if (tmpBarrel != null)
+        // {
+        //     tmpBarrel.transform.position = barrelPos.position;
+        //     tmpBarrel.transform.rotation = Quaternion.identity;
+        //     tmpBarrel.GetComponent<Rigidbody2D>().AddForce(barrelDirection);
+        // }
+
         yield return new WaitForSeconds(3f);
         StartCoroutine("createBarrel");
     }
